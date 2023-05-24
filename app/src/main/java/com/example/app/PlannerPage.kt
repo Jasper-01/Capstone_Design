cb4a362da4bbf4a9e82d02985025c47c0ac2465d
Jasper <jasperyap2001@gmail.com>
2023-05-25 02:16:16 +0900

    Initial UI design and colours. Portfolio page not implemented yet

--- app/src/main/java/com/example/app/PlannerPage.kt
+++ app/src/main/java/com/example/app/PlannerPage.kt
@@ -1,11 +1,59 @@
 package com.example.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.appcompat.app.AppCompatActivity
+
 
 class PlannerPage : AppCompatActivity() {
+    private val BUTTON_IDS = intArrayOf(
+        R.id.button00, R.id.button01, R.id.button02, R.id.button03, R.id.button04,
+        R.id.button10, R.id.button11, R.id.button12, R.id.button13, R.id.button14,
+        R.id.button20, R.id.button21, R.id.button22, R.id.button23, R.id.button24,
+        R.id.button30, R.id.button31, R.id.button32, R.id.button33, R.id.button34,
+        R.id.button40, R.id.button41, R.id.button42, R.id.button43, R.id.button44,
+    )
+
+    var buttons = arrayOfNulls<Button>(BUTTON_IDS.size)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planner_page)
+
+        val searchBtn = findViewById<LinearLayout>(R.id.searchBar)
+
+        for (i in BUTTON_IDS.indices){
+            buttons[i] = findViewById(BUTTON_IDS[i])
+            buttons[i]?.setOnClickListener{
+                val view = layoutInflater.inflate(R.layout.activity_input_popup, null)
+                val window = PopupWindow(this)
+                window.contentView = view
+                window.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+                val cancelBtn = view.findViewById<Button>(R.id.cancelBtn)
+                val applyBtn = view.findViewById<Button>(R.id.applyBtn)
+
+                cancelBtn.setOnClickListener {
+                    window.dismiss()
+                }
+
+                applyBtn.setOnClickListener{
+
+                }
+
+            }
+        }
+
+        searchBtn.setOnClickListener{
+            val view = layoutInflater.inflate(R.layout.activity_keyword_popup, null)
+            val window = PopupWindow(this)
+            window.contentView = view
+            window.showAtLocation(view, Gravity.CENTER,0, 0)
+
+
+        }
     }
 }
--- app/src/main/java/com/example/app/keywordPopup.kt
+++ app/src/main/java/com/example/app/keywordPopup.kt
@@ -1,8 +1,16 @@
 package com.example.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.appcompat.app.AppCompatActivity
+
 
 class keywordPopup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +18,10 @@ class keywordPopup : AppCompatActivity() {
         setContentView(R.layout.activity_keyword_popup)
 
         val searchBtn = findViewById<ImageButton>(R.id.searchImageButton)
+
+        searchBtn.setOnClickListener{
+            val Intent = Intent(this, inputPopup::class.java)
+            startActivity(Intent)
+        }
     }
 }
